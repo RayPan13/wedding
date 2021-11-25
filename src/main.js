ea5dcb9b3f2c58671a217c5ca0e100d3bcdb6b2c
RayPan <Ray0928066818@gmail.com>
2021-11-25 16:28:23 +0800

    安裝兩種字型 正楷、草寫

--- src/main.js
+++ src/main.js
@@ -9,3 +9,6 @@ export default function(Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.component('Layout', DefaultLayout)
 }
+//typeface font
+require('typeface-montserrat')
+require('typeface-dancing-script')
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,24 @@
 <template>
-    <h1>Gridsome !!</h1>
+    <div class="title">
+        <h3>Our Wedding Collection</h3>
+        <h1>Our Amazing Gallery</h1>
+    </div>
 </template>
 <style lang="scss" scoped>
+.title {
+    text-align: center;
+    padding-top: 24px;
+}
 h1 {
+    font-size: 5.6rem;
+    font-family: $cursive;
+    font-weight: 500;
+    margin: 0;
+}
+h3 {
+    font-size: 1.6rem;
     color: map-get($color, main);
-    @include media(768) {
-        font-size: 3.5rem;
-    }
-    @include media(480) {
-        font-size: 5.5rem;
-    }
+    font-weight: 500;
+    margin: 0 0 12px;
 }
 </style>
