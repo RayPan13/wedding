b906a385dbeca6d91e0283624e33f0c28435c1ca
RayPan <Ray0928066818@gmail.com>
2021-11-25 16:11:23 +0800

    add sass resources loader，共用 sass 變數

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,12 @@
 </template>
 <style lang="scss" scoped>
 h1 {
-    color: red;
+    color: map-get($color, main);
+    @include media(768) {
+        font-size: 3.5rem;
+    }
+    @include media(480) {
+        font-size: 5.5rem;
+    }
 }
 </style>
