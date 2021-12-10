d840ba562c6b9be66d64997536ce081db91477a0
RayPan <Ray0928066818@gmail.com>
2021-12-10 16:24:36 +0800

    add nav components

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,8 +1,26 @@
 <template>
-    <nav></nav>
+    <nav>
+        <ul>
+            <li v-for="item of menu" :key="item.txt">
+                <a :href="item.url">{{ item.txt }}</a>
+            </li>
+        </ul>
+    </nav>
 </template>
 <script>
 export default {
     name: 'TheNav',
+    data() {
+        return {
+            menu: [
+                { txt: 'HOME', url: '/' },
+                { txt: 'ABOUT', url: '/' },
+                { txt: 'RSVP', url: '/' },
+                { txt: 'GALLERY', url: '/' },
+                { txt: 'OUR STORY', url: '/' },
+                { txt: 'REVIEWS', url: '/' },
+            ],
+        }
+    },
 }
 </script>
