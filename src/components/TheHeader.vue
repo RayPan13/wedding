ca8bdf5bb585fce6f02d9350f09f6d827fca5bf6
RayPan <Ray0928066818@gmail.com>
2021-12-10 16:08:53 +0800

    整理分支

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,10 +3,13 @@
         <div class="logo">
             <fa :icon="['fas', 'gem']" />
         </div>
+        <the-nav />
     </header>
 </template>
 <script>
+import TheNav from '@/components/TheNav.vue'
 export default {
+    components: { TheNav },
     name: 'TheHeader',
 }
 </script>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav></nav>
+</template>
+<script>
+export default {
+    name: 'TheNav',
+}
+</script>
